6223dec7bcd9b1646b74195ca25ead49c4beeadb
axmedddddddd <arbuliew.akhmed@gmail.com>
2024-03-29 16:34:46 +0300

    fix: empty string bug

--- GraphMaker/graphmaker/cabinet_adapter.py
+++ GraphMaker/graphmaker/cabinet_adapter.py
@@ -17,7 +17,7 @@ logging.basicConfig(filename='backend.log', level=logging.INFO)
 logger = logging.getLogger(name)
 
 
-def api_call(method: str, str_param: str) -> Optional[Dict[str, Any]]:
+def api_call(method: str, str_param: str="") -> Optional[Dict[str, Any]]:
     """Accessing the Cabinet API"""
     if str_param:
         url = f"{base_url}/{method}/{str_param}"
